1dcbf16e249bd388d12c0e87ed5d9cc60717a785
Mason Dierkes <mason@seedinnovate.com>
2025-02-04 11:34:18 -0800

    Break ContentView with compile error

--- Sources/ContentView.swift
+++ Sources/ContentView.swift
@@ -2,7 +2,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        // Introducing compile error by returning multiple top-level views without a container
         Text("Hello, World!")
+        Text("This will cause a compile error")
             .padding()
     }
 }
@@ -11,4 +13,4 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
-} 
+}
